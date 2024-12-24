ad2789499241fc6cd38372bf25980bf7a281a21e
arlan <241217@astanait.edu.kz>
2024-12-24 11:20:48 +0500

    Task 1: Base Class Implementation

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        person p = new person("arlam", "aidarov", 10, true);
+        System.out.println(p.toString());
+
+    }
+}
---
+++ src/person.java
@@ -0,0 +1,55 @@
+public class person {
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+
+
+    public person(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        String genderString = gender ? "Male" : "Female";
+        return "hi i am " + name + " " + surname + ", a " + age + " year old, " + "im a " + genderString + ".";
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+
+}
